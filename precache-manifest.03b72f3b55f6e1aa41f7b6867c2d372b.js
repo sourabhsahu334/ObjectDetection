1ef6b1eb231c3dd9c50f284395bb63a90b8c797f
priyanshu1402 <priyanshuraghuvanshi1420@gmail.com>
2023-01-03 22:33:09 +0530

    Updates

---
+++ precache-manifest.03b72f3b55f6e1aa41f7b6867c2d372b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123db491bf3f14e96878a019d07483ba",
+    "url": "/ObjectDetection/index.html"
+  },
+  {
+    "revision": "5eff69eee86a2b5df8a7",
+    "url": "/ObjectDetection/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "33a62b99d2df15edb74b",
+    "url": "/ObjectDetection/static/js/2.761c83b3.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/ObjectDetection/static/js/2.761c83b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eff69eee86a2b5df8a7",
+    "url": "/ObjectDetection/static/js/main.8e8fad69.chunk.js"
+  },
+  {
+    "revision": "c68fc2cf57fb05b60363",
+    "url": "/ObjectDetection/static/js/runtime-main.5d25bc10.js"
+  }
+]);
